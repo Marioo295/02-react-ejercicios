686905d80f56625f28e780f5dfb0b98dccd984b5
Marioo295 <mramosgaliana@gmail.com>
2022-09-22 09:54:09 +0200

    Creación de json-server y primeras pruebas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import CrudApp from "./components/CrudApp"
+import CrudApi from './components/CrudApi';
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
-      <CrudApp></CrudApp>
       <hr></hr>
+      <CrudApi></CrudApi>
+      <hr></hr>
+      <CrudApp></CrudApp>
+
     </>
   );
 }
