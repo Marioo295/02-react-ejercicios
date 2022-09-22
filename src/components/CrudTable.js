ae3c9bbe00663f44c61aa95f1132d7f1dbf8c231
Marioo295 <mramosgaliana@gmail.com>
2022-09-22 13:56:52 +0200

    Peticiones HTTP a las API

--- src/components/CrudTable.js
+++ src/components/CrudTable.js
@@ -14,7 +14,13 @@ const CrudTable = ({data, setDataToEdit, deleteData}) => {
                 </tr>
             </thead>
             <tbody>
-                {data.length === 0 ? (<tr><td colSpan="3">Sin datos</td></tr>):(data.map((el)=><CrudTableRow key={el.id} el={el} setDataToEdit={setDataToEdit} deleteData={deleteData}></CrudTableRow>))}
+                {data.length > 0 ? 
+                    (data.map((el)=>
+                        <CrudTableRow key={el.id} el={el} setDataToEdit={setDataToEdit} deleteData={deleteData}></CrudTableRow>)
+                        ) : (
+                        <tr><td colSpan="3">Sin datos</td></tr>
+                    )
+                }
             </tbody>
         </table>
     </div>
