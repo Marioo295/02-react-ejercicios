31871e89aa34e72491ddbde494fbd63df5db28c2
Marioo295 <mramosgaliana@gmail.com>
2022-09-26 17:47:39 +0200

    Envío de datos AJAX y API FormSubmit

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,35 +1,59 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 
 export const useForm = (initialForm, validateForm) => {
-    const [form, setForm] = useState(initialForm);
-    const [errors, setErrors] = useState({});
-    const [loading, setLoading] = useState(false);
-    const [response, setResponse] = useState(null);
+  const [form, setForm] = useState(initialForm);
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState(null);
 
-    const handleChange = (e) => {
-        const{name, value} = e.target;
-        setForm({
-            ...form,
-            [name]: value
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleBlur = (e) => {
+    handleChange(e);
+    setErrors(validateForm(form));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validateForm(form));
+
+    if (Object.keys(errors).length === 0) {
+      alert("Enviando Formulario");
+      setLoading(true);
+      helpHttp()
+        .post("https://formsubmit.co/ajax/martamagant@gmail.com", {
+          body: form,
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        })
+        .then((res) => {
+          setLoading(false);
+          setResponse(true);
+          setForm(initialForm);
+          setTimeout(() => setResponse(false), 5000);
         });
-    };
+    } else {
+      return;
+    }
+  };
 
-    const handleBlur = (e) => {
-        handleChange(e);
-        setErrors(validateForm(form));
-    };
-    
-    const handleSubmit = (e) => {
-
-    };
-
-    return {
-        form,
-        errors, 
-        loading, 
-        response, 
-        handleChange, 
-        handleBlur, 
-        handleSubmit,
-    };
+  return {
+    form,
+    errors,
+    loading,
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  };
 };
