38f12a7e2e09f34eb1bd36d91ea4d2dfb2c189d4
Marioo295 <mramosgaliana@gmail.com>
2022-09-27 10:30:03 +0200

    Ventana modal: estilos y lógica del componente

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,22 +1,26 @@
 import React from 'react'
+import { useModal } from '../hooks/useModal';
 import Modal from './Modal'
 
 const Modals = () => {
+    const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+    const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+
     return (
         <div>
             <h2>MODALES</h2>
-            <button>Modal 1</button>
+            <button>Primer modal</button>
             <Modal>
                 <h3>Modal 1</h3>
-                <p>Hola este es el contenido de mi modal 1</p>
+                <p>Podemos ver animales muy bonitos</p>
                 <img src="https://placeimg.com/400/400/animals" alt="Animals"></img>
             </Modal>
-            <button>Modal 2</button>
-            <Modal>
+            <button>Segundo modal</button>
+            {/* <Modal>
                 <h3>Otro modal</h3>
                 <p>Lorem ipsum</p>
                 <img src="https://placeimg.com/400/400/nature" alt="Nature"></img>
-            </Modal>
+            </Modal> */}
         </div>
     );
 };
