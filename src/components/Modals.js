07fed9a0a4917a414a70fcba6daa6c8150099f51
Marioo295 <mramosgaliana@gmail.com>
2022-09-27 10:52:31 +0200

    Ventana modal: funcionalidad

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import { useModal } from '../hooks/useModal';
 import Modal from './Modal'
+import FormContact from './FormContact'
 
 const Modals = () => {
     const [isOpenModal1, openModal1, closeModal1] = useModal(false);
     const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+    const [isOpenModalContact, openModalContact, closeModalContact] = useModal(false);
 
     return (
         <div>
             <h2>MODALES</h2>
-            <button>Primer modal</button>
-            <Modal>
-                <h3>Modal 1</h3>
+            <button onClick={openModal1}>1º modal</button>
+            <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
+                <h3>Animales</h3>
                 <p>Podemos ver animales muy bonitos</p>
                 <img src="https://placeimg.com/400/400/animals" alt="Animals"></img>
             </Modal>
-            <button>Segundo modal</button>
-            {/* <Modal>
-                <h3>Otro modal</h3>
-                <p>Lorem ipsum</p>
+            <button onClick={openModal2}>2º modal</button>
+            <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
+                <h3>Paisajes</h3>
+                <p>Podemos ver paisajes muy bonitos</p>
                 <img src="https://placeimg.com/400/400/nature" alt="Nature"></img>
-            </Modal> */}
+            </Modal>
+            <button onClick={openModalContact}>Modal de contacto</button>
+            <Modal isOpen={isOpenModalContact} closeModal={closeModalContact}>
+                <FormContact></FormContact>
+            </Modal>
         </div>
     );
 };
